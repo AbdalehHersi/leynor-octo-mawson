c1c6a83bd56e3fad2c9f4a9e45ac3406c3fbd956
Abdaleh Hersi <therealhersi03@gmail.com>
2022-07-28 16:39:04 +0100

    added score in highscore.js

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,1 +1,13 @@
-console.log(localStorage.getItem('test'));
+var retrievedObject = JSON.parse(localStorage.getItem("userInfo"));
+var nameId = document.querySelector("#name-ul");
+console.log(retrievedObject);
+var nameEL = document.createElement("li");
+nameEL.classList.add("name-li");
+nameEL.textContent = retrievedObject.userNameObject;
+nameId.append(nameEL);
+
+var highId = document.querySelector("#high-ul");
+var highEl = document.createElement("li");
+highEl.classList.add("high-li");
+highEl.textContent = "scored " + retrievedObject.scoreObject + "/5, with a time of " + retrievedObject.timerCountObject + "s";
+highId.append(highEl);
